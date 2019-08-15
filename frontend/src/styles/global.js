c8acfb4fee3bafd2e9699004a7fc653066f17721
GugaDavi <gguedes.tech@gmail.com>
2019-08-15 02:41:07 -0300

    Reset CSS

---
+++ frontend/src/styles/global.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoonthing: antialiased;
+    background: linear-gradient(180deg, #22202C 0%, #402845 100%);
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
