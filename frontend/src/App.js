d59045dacfc7ba3042b6eb234a6ae0afe848c406
GugaDavi <gguedes.tech@gmail.com>
2019-08-15 02:38:39 -0300

    Config Eslint e Ajustes Iniciais

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import Routes from './routes';
+import history from './services/history';
+
+import GlobalStyles from './styles/global';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </>
+  );
 }
 
 export default App;
