87e7bd9373ab57d8d51f62e8f5caa968e3328dcc
GugaDavi <gguedes.tech@gmail.com>
2019-08-15 21:08:06 -0300

    Redux Config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import Routes from './routes';
 import history from './services/history';
+
+import './config/ReactotronConfig';
+import { store, persistor } from './store';
 
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyles />
-      </Router>
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyles />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
