3de48808c80d5a805d1be80dfd2b3b5f956d9094
GugaDavi <gguedes.tech@gmail.com>
2019-09-01 04:01:59 -0300

    Input Element

---
+++ mobile/src/components/Input/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding: 0px 20px;
+  height: 50px;
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TInput = styled.TextInput.attrs({
+  placeholderTextColor: 'rgba(255, 255, 255, 0.6)',
+})`
+  flex: 1;
+  font-size: 18px;
+  line-height: 21px;
+  margin-left: 10px;
+  color: #fff;
+`;
