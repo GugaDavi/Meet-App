e9be389006cf4ddaaaa3e9ed052504a93755280f
GugaDavi <gguedes.tech@gmail.com>
2019-09-01 00:33:06 -0300

    Rotas

---
+++ mobile/src/routes.js
@@ -0,0 +1,45 @@
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+import Dashboard from './pages/Dashboard';
+import Inscription from './pages/Inscription';
+import Profile from './pages/Profile';
+
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Inscription,
+            Profile,
+          },
+          {
+            resetOnBlur: true,
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#2B1A2F',
+              },
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
