6ce12513e4f5a014e45836b2619e843df0259fd6
GugaDavi <gguedes.tech@gmail.com>
2019-09-01 04:03:11 -0300

    Inscription Page

--- mobile/src/pages/Inscription/index.js
+++ mobile/src/pages/Inscription/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 // import { Container } from './styles';
 
 export default function Inscription() {
   return <View />;
 }
+
+Inscription.navigationOptions = {
+  tabBarLabel: 'Incrições',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="local-offer" size={20} color={tintColor} />
+  ),
+};
